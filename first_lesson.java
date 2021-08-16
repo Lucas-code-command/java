8815f2dda2aedeb005f92f234475369468bb0277
Lucas Moliterno <lucasmoliterno@Lucass-MacBook-Pro.local>
2021-08-16 13:29:58 -0300

    Update first_lesson.java

---
+++ first_lesson.java
@@ -0,0 +1,19 @@
+class Myclass {
+    public static void main (String[ ] args){
+        String name = "Lucas";
+        int age = 20;
+        char group = 'M';
+        boolean estuda = true;
+        System.out.println(name);
+    }}
+
+/* 
+char stands for character and holds a single character.
+boolean x = false; 
+Double numero_complexo = 10.001;
+*/
+
+String name = "Lucas";
+Integer numero = 10;
+
+
