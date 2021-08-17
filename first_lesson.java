2819280780b5895bfcc03e324297651a77569514
Lucas Moliterno <lucas.moliterno1@gmail.com>
2021-08-16 21:14:59 -0300

    commit

--- first_lesson.java
+++ first_lesson.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Myclass {
     public static void main (String[ ] args){
         String name = "Lucas";
@@ -16,3 +18,20 @@ boolean x = false;
 Double numero_complexo = 10.001;
 */
 
+class Input {
+    public static void x(String[ ] args){
+        Scanner inputOne = new Scanner(System.in);
+        System.out.println(inputOne.nextLine());
+    }
+}
+
+// Modulo retorna aquilo que sobrou da divisão
+
+class Numeros {
+    public static void y (String[ ] args) {
+        Scanner inputTwo = new Scanner(System.in);
+        int input = inputTwo.nextInt();
+        int result = input / 10;
+        System.out.println(result);
+    }
+}
