649e78502a3f8326cd1673af816435b1b00c16e1
Lucas Moliterno <lucas.moliterno1@gmail.com>
2021-08-18 13:55:18 -0300

    commir

---
+++ lesson.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.nextLine();
+		char[] arr = text.toCharArray();
+		
+		//your code goes here
+        int[] teste = {1,2,3,4,5};
+        //teste[i+teste.length] => 1 => 6
+        //decrescer 2 => precisa ser 5
+        for (int i=0 ; i<teste.length ; i++){
+            System.out.println();      
+        }
+        
+	}
+}
