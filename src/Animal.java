a85b732db443a048c624a821d2133aeb6f6e9844
Lucas Moliterno <lucas.moliterno1@gmail.com>
2021-08-24 16:50:38 -0300

    commit

--- src/Animal.java
+++ src/Animal.java
@@ -1,9 +1,6 @@
+package src;
 public class Animal {
     void bark(){
         System.out.println("au-au-au");
-    }
-
-    public static void main(String args[]){
-        bark();
     }
 }
---
+++ src/main.java
@@ -0,0 +1,8 @@
+package src;
+
+class Main {
+    public static void main(String args[]){
+        Animal dog = new Animal();
+        dog.bark();
+    }
+}
